78f53e8f7a67e6f4eb83fe9a0c131fad7d0d73eb
Johan Norberg <lonezor@gmail.com>
2025-09-13 13:42:33 +0100

    src: Add support for RP2040-ETH
    Board info:
    https://www.waveshare.com/wiki/RP2040-ETH
    
    The source code has been modified to match use case. Driver code switch from TCP to UDP.
    Configuration is not hard coded inside but via the API.
    
    Building/flashing using Arduino IDE 1.8.19. Note that custom board manager according to
    above URL is needed.

---
+++ src/RP2040-ETH/RP2040-ETH.ino
@@ -0,0 +1,44 @@
+#include "CH9120.h"
+
+UCHAR CH9120_LOCAL_IP[4] = {192, 168, 201, 206};
+UCHAR CH9120_GATEWAY[4] = {192, 168, 201, 1};
+UCHAR CH9120_SUBNET_MASK[4] = {255, 255, 255, 0};
+UCHAR CH9120_REMOTE_IP[4] = {192, 168, 201, 101};
+UWORD CH9120_LOCAL_PORT = 1001;
+UWORD CH9120_REMOTE_PORT = 2001;
+
+volatile int adc_value = 0;
+volatile int32_t iteration = 0;
+
+void setup() {
+    Serial.begin(115200);
+  
+    CH9120_init(CH9120_LOCAL_IP,
+                CH9120_GATEWAY,
+                CH9120_SUBNET_MASK,
+                CH9120_REMOTE_IP,
+                CH9120_LOCAL_PORT,
+                CH9120_REMOTE_PORT);
+}
+
+
+void setup1() {
+  analogReadResolution(12); 
+}
+
+void loop() {
+  char msg[128];
+  snprintf(msg, sizeof(msg), "Iteration %d, adc_value %d\n", iteration, adc_value);
+  
+  Serial.print(msg);
+  SendUdpPacket(msg);
+  
+  delay(250);
+}
+
+
+
+void loop1() {
+  iteration++;
+  adc_value = analogRead(A0);
+}
