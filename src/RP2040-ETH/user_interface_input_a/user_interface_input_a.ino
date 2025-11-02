bb5ed4a7c9f1454b7e8135c37b78615bf8638331
Johan Norberg <lonezor@gmail.com>
2025-11-02 19:31:40 +0000

    User interface input #1: measure RMS samples analog input

--- src/RP2040-ETH/user_interface_input_a/user_interface_input_a.ino
+++ src/RP2040-ETH/user_interface_input_a/user_interface_input_a.ino
@@ -8,10 +8,14 @@ UWORD CH9120_LOCAL_PORT = 10;
 UWORD CH9120_REMOTE_PORT = 10;
 
 volatile int adc_value = 0;
+volatile int rms_sample_index = 0;
 volatile int32_t iteration = 0;
+
+int block_size = 4096 / 15;
 
 void setup() {
     Serial.begin(115200);
+    Serial.println("User Interface Input Module #1");
   
     CH9120_init(CH9120_LOCAL_IP,
                 CH9120_GATEWAY,
@@ -28,7 +32,7 @@ void setup1() {
 
 void loop() {
   char msg[128];
-  snprintf(msg, sizeof(msg), "Iteration %d, adc_value %d\n", iteration, adc_value);
+  snprintf(msg, sizeof(msg), "Iteration %d, adc_value %d, index %d\n", iteration, adc_value, rms_sample_index);
   
   Serial.print(msg);
   SendUdpPacket(msg);
@@ -41,4 +45,7 @@ void loop() {
 void loop1() {
   iteration++;
   adc_value = analogRead(A0);
+
+  rms_sample_index = adc_value / block_size;
+  
 }
